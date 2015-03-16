abac6503201d4e2ed009e55cd60ed653712233ad
jasonbutler42 <jasonbutler42@gmail.com>
2015-03-16 13:24:28 -0500

    fix(wfcache): add linked directory for WordFence caching

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,7 +20,7 @@ set :log_level, :info
 set :linked_files, %w{.env web/.htaccess web/wp/.htaccess web/robots.txt web/dumbass_robots/blackhole.dat}
 # Linked directories
 # set :linked_dirs, %w{web/app/uploads web/app/w3tc-config web/app/cache web/app/aiowps_backups web/app/plugins/ad-injection-data web/app/plugins/fanciest-author-box web/app/plugins/pinterest-pin-it}
-set :linked_dirs, %w{web/app/uploads}
+set :linked_dirs, %w{web/app/uploads web/app/wfcache}
 
 namespace :deploy do
   desc 'Restart application'
