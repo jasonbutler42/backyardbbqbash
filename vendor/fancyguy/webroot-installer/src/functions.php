4d355b4686278bf77f114d606f98891b78d369ef
jasonbutler42 <jasonbutler42@gmail.com>
2015-04-02 14:10:15 -0500

    fix(plugins): cleanup time!

--- vendor/fancyguy/webroot-installer/src/functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function includeIfExists($file)
-{
-    if (file_exists($file)) {
-        $return = include $file;
-        return $return;
-    }
-}
--- web/app/plugins/w3tc-wp-loader.php
+++
@@ -1,4 +0,0 @@
-
-<?php
-    if (W3TC_WP_LOADING)
-        require_once '/workspace/jasonbutler42/web/wp/wp-load.php';
